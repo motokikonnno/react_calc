462a27efcdf28561437441969273b94a728c2e4f
Motoki <80935829+motokikonnno@users.noreply.github.com>
2022-08-15 18:38:40 +0900

    first commit

---
+++ src/Calculator.tsx
@@ -0,0 +1,17 @@
+import ButtonPanel from "./ButtonPanel";
+import Display from "./Display";
+
+
+export default function Calculator() {
+
+    const buttonHandler = (code: string) => {
+        console.log(code)
+    }
+
+    return (
+        <div>
+            <Display />
+            <ButtonPanel buttonHandler={buttonHandler} />
+        </div>
+    );
+}
